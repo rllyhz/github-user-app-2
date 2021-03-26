9a7afeab9881005716de9ec53ac57b9d0271932f
rllyhz <rullyihza00@gmail.com>
2021-03-27 00:45:46 +0700

    Creating Profile UI

---
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package id.rllyhz.githubuserapp.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import id.rllyhz.githubuserapp.databinding.FragmentHomeBinding
+import id.rllyhz.githubuserapp.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!! // this approach is from official documentation
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null // avoiding memory leaks
+    }
+}

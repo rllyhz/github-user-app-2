dfd6ed4db19e2236e081d993e3a25632b39c859d
rllyhz <rullyihza00@gmail.com>
2021-03-26 23:37:47 +0700

    Creating model class, named User

---
+++ app/src/main/java/id/rllyhz/githubuserapp/data/model/User.kt
@@ -0,0 +1,18 @@
+package id.rllyhz.githubuserapp.data.model
+
+data class User(
+    val id: Int,
+    val username: String,
+    val avatarUrl: String,
+    var fullName: String = "",
+    val companyName: String = "",
+    val blogUrl: String = "",
+    val location: String = "",
+    val email: String = "",
+    val twitterUsername: String = "",
+    val bio: String = "",
+    val repositoriesCount: Int = 0,
+    val gistsCount: Int = 0,
+    val followersCount: Int = 0,
+    val followingCount: Int = 0
+)

cfb2e1f060795ab11874bc0e3564270a834ec1fd
rllyhz <rullyihza00@gmail.com>
2021-03-27 01:32:01 +0700

    Renaming labels of navigation components respectively

--- app/src/main/java/id/rllyhz/githubuserapp/ui/MainActivity.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/MainActivity.kt
@@ -3,10 +3,18 @@ package id.rllyhz.githubuserapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import id.rllyhz.githubuserapp.R
 import id.rllyhz.githubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +24,19 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // get navController from the nav host
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.apply {
+            setupActionBarWithNavController(navController)
+            bottomNavigationView.setupWithNavController(navController)
+        }
     }
 }

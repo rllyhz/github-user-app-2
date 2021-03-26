859d1ed1b669ac9ec6c8dfdf4b70ec7608418502
rllyhz <rullyihza00@gmail.com>
2021-03-27 01:15:10 +0700

    Setting up view binding in MainActivity.kt file

--- app/src/main/java/id/rllyhz/githubuserapp/ui/MainActivity.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package id.rllyhz.githubuserapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import id.rllyhz.githubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // this app doesn't support dark mode yet for now
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }

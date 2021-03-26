fced29bb2bdb34fcea2e27c7dec4bccfbbead7ed
rllyhz <rullyihza00@gmail.com>
2021-03-26 23:32:15 +0700

    Creating HomeViewModel.kt file

--- app/src/main/java/id/rllyhz/githubuserapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/home/HomeViewModel.kt
@@ -1,4 +1,20 @@
 package id.rllyhz.githubuserapp.ui.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.rllyhz.githubuserapp.api.GithubApi
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel // this is the different between other class
+class HomeViewModel @Inject constructor( // @ViewModelInject is deprecated for latest version
+    api: GithubApi
+) : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            val users = api.getUsers()
+        }
+    }
 }

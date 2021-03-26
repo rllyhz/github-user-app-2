2746f37bb96e94589cc0ded2a2fbadbd566153c3
rllyhz <rullyihza00@gmail.com>
2021-03-26 23:45:03 +0700

    Updating return value of endpoints GithubApi.kt

--- app/src/main/java/id/rllyhz/githubuserapp/api/GithubApi.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/api/GithubApi.kt
@@ -3,7 +3,6 @@ package id.rllyhz.githubuserapp.api
 import id.rllyhz.githubuserapp.data.response.SearchUsersResponse
 import id.rllyhz.githubuserapp.data.response.UserDetailResponse
 import id.rllyhz.githubuserapp.data.response.UsersResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -15,11 +14,11 @@ interface GithubApi {
     }
 
     @GET("users")
-    suspend fun getUsers(): Call<List<UsersResponse>>
+    suspend fun getUsers(): List<UsersResponse>
 
     @GET("users/{username}")
-    suspend fun getUserDetailOf(@Path("username") username: String) : Call<UserDetailResponse>
+    suspend fun getUserDetailOf(@Path("username") username: String): UserDetailResponse
 
     @GET("search/users")
-    suspend fun searchUsers(@Query("q") query: String) : Call<SearchUsersResponse>
+    suspend fun searchUsers(@Query("q") query: String): SearchUsersResponse
 }

c5304dac5376a284a7328d9d67054157f76fbcd8
rllyhz <rullyihza00@gmail.com>
2021-03-26 23:00:12 +0700

    Creating provideRetrofitInstance() method in AppModule.kt file for providing Retrofit instance using Dagger Hilt

--- app/src/main/java/id/rllyhz/githubuserapp/di/AppModule.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/di/AppModule.kt
@@ -1,4 +1,18 @@
 package id.rllyhz.githubuserapp.di
 
-class AppModule {
+import dagger.Provides
+import id.rllyhz.githubuserapp.api.GithubApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance() : Retrofit =
+        Retrofit.Builder()
+            .baseUrl(GithubApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 }

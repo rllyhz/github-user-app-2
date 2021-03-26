e2f0e5af85334002debed4c1c4a11fc049273dfe
rllyhz <rullyihza00@gmail.com>
2021-03-27 04:05:09 +0700

    - Fixing up ResourceEvent.kt file

--- app/src/main/java/id/rllyhz/githubuserapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/home/HomeViewModel.kt
@@ -3,13 +3,10 @@ package id.rllyhz.githubuserapp.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import id.rllyhz.githubuserapp.api.GithubApi
-import id.rllyhz.githubuserapp.data.model.User
 import id.rllyhz.githubuserapp.repository.main.MainRepository
 import id.rllyhz.githubuserapp.util.DispacherProvider
-import kotlinx.coroutines.launch
+import id.rllyhz.githubuserapp.util.ResourceEvent
 import javax.inject.Inject
 
 @HiltViewModel // this is the different between other class
@@ -17,6 +14,7 @@ class HomeViewModel @Inject constructor( // @ViewModelInject is deprecated for l
     private val mainRepository: MainRepository,
     private val dispachers: DispacherProvider
 ) : ViewModel() {
-    private val usersLiveData = MutableLiveData<List<User>>()
-    val users: LiveData<List<User>> = usersLiveData
+
+    private val _users = MutableLiveData<ResourceEvent>(ResourceEvent.Empty)
+    val users: LiveData<ResourceEvent> = _users
 }
--- app/src/main/java/id/rllyhz/githubuserapp/util/ResourceEvent.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/util/ResourceEvent.kt
@@ -2,7 +2,7 @@ package id.rllyhz.githubuserapp.util
 
 sealed class ResourceEvent {
     class Success<T>(result: T) : ResourceEvent()
-    class Error(message: String) : ResourceEvent()
+    class Failure(message: String) : ResourceEvent()
     object Loading : ResourceEvent()
     object Empty : ResourceEvent()
 }

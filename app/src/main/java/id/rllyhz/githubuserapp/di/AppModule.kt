c945b2fee116d91596067bdae0c629f03005fe85
rllyhz <rullyihza00@gmail.com>
2021-03-26 23:08:43 +0700

    Specifying the module (AppModule.kt) so that it lives in Application scope

--- app/src/main/java/id/rllyhz/githubuserapp/di/AppModule.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/di/AppModule.kt
@@ -1,11 +1,16 @@
 package id.rllyhz.githubuserapp.di
 
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import id.rllyhz.githubuserapp.api.GithubApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class) // application component deprecated in latest version
 object AppModule {
 
     @Provides

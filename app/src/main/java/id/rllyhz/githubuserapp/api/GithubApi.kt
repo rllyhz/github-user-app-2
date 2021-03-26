5c718dfd1b17ff8c9428a6d333fed39bcb1055ec
rllyhz <rullyihza00@gmail.com>
2021-03-26 22:38:58 +0700

    Adding some endpoints in GithubApi.kt, which are getUsers() and getUserDetailOf()

--- app/src/main/java/id/rllyhz/githubuserapp/api/GithubApi.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/api/GithubApi.kt
@@ -1,8 +1,21 @@
 package id.rllyhz.githubuserapp.api
 
+import id.rllyhz.githubuserapp.data.response.UserDetailResponse
+import id.rllyhz.githubuserapp.data.response.UsersResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface GithubApi {
 
     companion object {
         const val BASE_URL = "https://api.github.com/"
     }
+
+    @GET("users")
+    suspend fun getUsers(): Call<List<UsersResponse>>
+
+    @GET("users/{username}")
+    suspend fun getUserDetailOf(@Path("username") username: String) : Call<UserDetailResponse>
 }

98a2ba9f499c699c8049f1c9fb27ca2351b06753
rllyhz <rullyihza00@gmail.com>
2021-03-27 04:01:07 +0700

    - Fixing up ResourceEvent.kt file

--- app/src/main/java/id/rllyhz/githubuserapp/util/ResourceEvent.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/util/ResourceEvent.kt
@@ -3,6 +3,6 @@ package id.rllyhz.githubuserapp.util
 sealed class ResourceEvent {
     class Success<T>(result: T) : ResourceEvent()
     class Error(message: String) : ResourceEvent()
-    class Loading : ResourceEvent()
-    class Empty : ResourceEvent()
+    object Loading : ResourceEvent()
+    object Empty : ResourceEvent()
 }

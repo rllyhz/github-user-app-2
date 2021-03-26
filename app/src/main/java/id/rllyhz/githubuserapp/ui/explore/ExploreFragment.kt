21892149b832702fb556c0b2be757bda2ac3a0d8
rllyhz <rullyihza00@gmail.com>
2021-03-27 00:57:34 +0700

    Refactoring Search UI become Explore UI - SearchFragment.kt => ExploreFragment.kt - fragment_search.xml => fragment_explore.xml

--- app/src/main/java/id/rllyhz/githubuserapp/ui/explore/ExploreFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/explore/ExploreFragment.kt
@@ -1,14 +1,14 @@
-package id.rllyhz.githubuserapp.ui.search
+package id.rllyhz.githubuserapp.ui.explore
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import id.rllyhz.githubuserapp.databinding.FragmentSearchBinding
+import id.rllyhz.githubuserapp.databinding.FragmentExploreBinding
 
-class SearchFragment : Fragment() {
-    private var _binding: FragmentSearchBinding? = null
+class ExploreFragment : Fragment() {
+    private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!! // this approach is from official documentation
 
     override fun onCreateView(
@@ -16,7 +16,7 @@ class SearchFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
         return binding.root
     }
 

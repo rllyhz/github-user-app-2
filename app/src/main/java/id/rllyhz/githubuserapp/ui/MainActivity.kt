b4c8df2c2347899d827a0371eecdcdb8aee84e8e
rllyhz <rullyihza00@gmail.com>
2021-03-26 20:51:39 +0700

    Refactoring MainActivity.kt files - Moving the MainActivity.kt file from id/rllyhz/githubuserapp/MainActivity.kt (in the root package) to id/rllyhz/githubuserapp/ui/MainActivity.kt

--- app/src/main/java/id/rllyhz/githubuserapp/ui/MainActivity.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.rllyhz.githubuserapp
+package id.rllyhz.githubuserapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.rllyhz.githubuserapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

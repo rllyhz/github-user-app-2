06f446f070efcc84186d647be7bd796fb4d3948c
rllyhz <rullyihza00@gmail.com>
2021-03-26 23:22:29 +0700

    Creating Home UI

--- app/src/main/java/id/rllyhz/githubuserapp/ui/home/HomeFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/home/HomeFragment.kt
@@ -1,5 +1,27 @@
 package id.rllyhz.githubuserapp.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import id.rllyhz.githubuserapp.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment()
+class HomeFragment: Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!! // this approach is from official documentation
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null // avoiding memory leaks
+    }
+}

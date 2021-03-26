338f9159e0787ed0284025763f9e2eea474dfb14
rllyhz <rullyihza00@gmail.com>
2021-03-27 05:27:56 +0700

    Updating ResourceEvent.kt

--- app/src/main/java/id/rllyhz/githubuserapp/util/ResourceEvent.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/util/ResourceEvent.kt
@@ -1,7 +1,7 @@
 package id.rllyhz.githubuserapp.util
 
 sealed class ResourceEvent {
-    class Success<T>(val resultList: List<T>, val result: T?) : ResourceEvent()
+    class Success<T>(val resultList: List<T>?, val result: T?) : ResourceEvent()
     class Failure(val message: String) : ResourceEvent()
     object Loading : ResourceEvent()
     object Empty : ResourceEvent()

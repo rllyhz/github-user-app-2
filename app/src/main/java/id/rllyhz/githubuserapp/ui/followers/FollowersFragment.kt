eb13094336b8827b2b6c4aaa436c1ae70eceb84e
rllyhz <rullyihza00@gmail.com>
2021-03-29 05:15:39 +0700

    Creating fragment_follows.xml file, layout UI for FollowingFragment and FollowersFragment

--- app/src/main/java/id/rllyhz/githubuserapp/ui/followers/FollowersFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/followers/FollowersFragment.kt
@@ -1,5 +1,54 @@
 package id.rllyhz.githubuserapp.ui.followers
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import id.rllyhz.githubuserapp.R
+import id.rllyhz.githubuserapp.data.model.User
+import id.rllyhz.githubuserapp.ui.home.UserListAdapter
+import id.rllyhz.githubuserapp.ui.user_detail.UserDetailActivity
 
-class FollowersFragment: Fragment()
+class FollowersFragment : Fragment(R.layout.fragment_follows), UserListAdapter.ItemClickCallback {
+    // for this class, no need view binding
+    private lateinit var recyclerView: RecyclerView
+    private var usersAdapter: UserListAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        usersAdapter = UserListAdapter()
+        usersAdapter?.setOnItemListener(this)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerview(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        usersAdapter = null
+    }
+
+    private fun setupRecyclerview(view: View) {
+        recyclerView = view.findViewById(R.id.recyclerview_following)
+
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = usersAdapter
+        }
+    }
+
+    override fun onDetailIconClick(user: User) {
+        Intent(requireActivity(), UserDetailActivity::class.java).run {
+            putExtra(UserDetailActivity.USER_EXTRAS, user)
+
+            requireActivity().startActivity(this)
+        }
+    }
+}
--- app/src/main/java/id/rllyhz/githubuserapp/ui/following/FollowingFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/following/FollowingFragment.kt
@@ -1,5 +1,6 @@
 package id.rllyhz.githubuserapp.ui.following
 
 import androidx.fragment.app.Fragment
+import id.rllyhz.githubuserapp.R
 
-class FollowingFragment: Fragment()
+class FollowingFragment: Fragment(R.layout.fragment_follows)

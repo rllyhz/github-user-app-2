152bbfe63a3553b39a56ac087b193f2a155568b0
rllyhz <rullyihza00@gmail.com>
2021-03-28 22:00:48 +0700

    Setting up the recyclerview in UserListAdapter.kt using DiffUtil

--- app/src/main/java/id/rllyhz/githubuserapp/ui/home/UserListAdapter.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/home/UserListAdapter.kt
@@ -1,4 +1,46 @@
 package id.rllyhz.githubuserapp.ui.home
 
-class UserListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import id.rllyhz.githubuserapp.data.model.User
+import id.rllyhz.githubuserapp.databinding.ItemUserBinding
+
+class UserListAdapter : ListAdapter<User, UserListAdapter.UserListViewHolder>(UserComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    inner class UserListViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(user.avatarUrl)
+                    .into(sivItemUserAvatar)
+
+                tvItemUserUsername.text = user.username
+            }
+        }
+    }
+
+    class UserComparator() : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean =
+            oldItem == newItem // cause already using data class
+    }
 }

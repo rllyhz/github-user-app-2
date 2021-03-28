f77e98d48364b4f197eb01c544f427441cb17f85
rllyhz <rullyihza00@gmail.com>
2021-03-29 02:59:07 +0700

    Creating FollowersFragment.kt and FollowingFragment.kt files

---
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/followers/FollowersFragment.kt
@@ -0,0 +1,5 @@
+package id.rllyhz.githubuserapp.ui.followers
+
+import androidx.fragment.app.Fragment
+
+class FollowersFragment: Fragment()
---
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/following/FollowingFragment.kt
@@ -0,0 +1,5 @@
+package id.rllyhz.githubuserapp.ui.following
+
+import androidx.fragment.app.Fragment
+
+class FollowingFragment: Fragment()

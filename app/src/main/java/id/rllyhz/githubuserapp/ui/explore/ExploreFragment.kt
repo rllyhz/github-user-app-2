8d89c1a8f0fb809e72ddde63d435272a37bf40f0
rllyhz <rullyihza00@gmail.com>
2021-03-30 07:05:15 +0700

    Updating Explore UI

--- app/src/main/java/id/rllyhz/githubuserapp/ui/explore/ExploreFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/explore/ExploreFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import id.rllyhz.githubuserapp.databinding.FragmentExploreBinding
 
@@ -18,6 +20,41 @@ class ExploreFragment : Fragment() {
     ): View? {
         _binding = FragmentExploreBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUI()
+    }
+
+    private fun setUI() {
+        binding.apply {
+            etExploreSearch.setOnEditorActionListener { etSearchView, actionId, _ ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_SEARCH -> {
+                        searchUsers(etSearchView.text.toString())
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            mcardExploreSearchIconContainer.setOnClickListener {
+                searchUsers(etExploreSearch.text.toString())
+            }
+
+            etExploreSearch.requestFocus()
+        }
+    }
+
+    private fun searchUsers(query: String) {
+        if (query.isEmpty())
+            return
+
+        Toast.makeText(context, "Searching...", Toast.LENGTH_SHORT).show()
+
+        binding.etExploreSearch.text.clear()
     }
 
     override fun onDestroy() {

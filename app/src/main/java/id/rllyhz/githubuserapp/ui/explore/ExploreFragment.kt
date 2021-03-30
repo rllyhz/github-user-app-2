a0a4f8d4656514e29901ca30fa6fd078d8d93b31
rllyhz <rullyihza00@gmail.com>
2021-03-30 08:41:26 +0700

    Updating Explore UI

--- app/src/main/java/id/rllyhz/githubuserapp/ui/explore/ExploreFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/explore/ExploreFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import id.rllyhz.githubuserapp.R
 import id.rllyhz.githubuserapp.databinding.FragmentExploreBinding
 
 class ExploreFragment : Fragment() {
@@ -30,6 +32,9 @@ class ExploreFragment : Fragment() {
 
     private fun setUI() {
         binding.apply {
+
+            setupInitialUI()
+
             etExploreSearch.setOnEditorActionListener { etSearchView, actionId, _ ->
                 when (actionId) {
                     EditorInfo.IME_ACTION_SEARCH -> {
@@ -44,6 +49,7 @@ class ExploreFragment : Fragment() {
                 searchUsers(etExploreSearch.text.toString())
             }
 
+            setSearchable(true)
             etExploreSearch.requestFocus()
         }
     }
@@ -54,7 +60,48 @@ class ExploreFragment : Fragment() {
 
         Toast.makeText(context, "Searching...", Toast.LENGTH_SHORT).show()
 
-        binding.etExploreSearch.text.clear()
+        binding.apply {
+            setSearchable(false)
+            etExploreSearch.text.clear()
+        }
+    }
+
+    private fun setSearchable(state: Boolean) {
+        binding.apply {
+            etExploreSearch.isEnabled = state
+            etExploreSearch.isFocusableInTouchMode = state
+        }
+    }
+
+    private fun setupFailureUI() {
+        binding.apply {
+            recyclerviewExplore.visibility = View.GONE
+            llIllustrationContainer.visibility = View.GONE
+        }
+    }
+
+    private fun setupSuccessUI() {
+        binding.apply {
+            recyclerviewExplore.visibility = View.VISIBLE
+            llIllustrationContainer.visibility = View.GONE
+        }
+    }
+
+    private fun setupInitialUI() {
+        binding.apply {
+            recyclerviewExplore.visibility = View.GONE
+            llIllustrationContainer.visibility = View.VISIBLE
+
+            swipeRefreshExplore.setColorSchemeColors(
+                ContextCompat.getColor(requireContext(), R.color.redish_500),
+                ContextCompat.getColor(requireContext(), R.color.blue_200),
+                ContextCompat.getColor(requireContext(), R.color.blue_500)
+            )
+
+            swipeRefreshExplore.setOnRefreshListener {
+                swipeRefreshExplore.isRefreshing = false
+            }
+        }
     }
 
     override fun onDestroy() {

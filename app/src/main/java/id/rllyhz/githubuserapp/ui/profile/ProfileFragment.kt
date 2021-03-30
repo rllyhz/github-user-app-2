0f95b3cbaf89a0440f27b2ba19a6bd79afe270bf
rllyhz <rullyihza00@gmail.com>
2021-03-30 11:17:46 +0700

    Cleaning up the codes

--- app/src/main/java/id/rllyhz/githubuserapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/id/rllyhz/githubuserapp/ui/profile/ProfileFragment.kt
@@ -27,7 +27,7 @@ class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!! // this approach is from official documentation
 
-    private var mAdapter: FollowingFollowersPagerAdapter? = null
+    private lateinit var mAdapter: FollowingFollowersPagerAdapter
 
     private val viewModel: ProfileViewModel by viewModels()
 
@@ -168,6 +168,5 @@ class ProfileFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null // avoiding memory leaks
-        mAdapter = null
     }
 }
